1859c3e91268fb28484a17feef6eb44953fe2ebe
bigfishh <anniezheng.anniee@gmail.com>
2020-03-31 12:32:00 -0400

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 5) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "item_name"
+  end
 
   create_table "orders", force: :cascade do |t|
     t.string "user_id"
     t.string "starbucks_id"
+    t.string "items"
   end
 
   create_table "starbucks", force: :cascade do |t|
